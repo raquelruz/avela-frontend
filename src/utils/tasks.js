457fc948ad72542c2017db799499d7425d3e1691
Raquel Ruiz <raquelruizlopezcepero@gmail.com>
2026-09-03 17:37:40 +0200

    feature/home: add first structure for tasks

---
+++ src/utils/tasks.js
@@ -0,0 +1,37 @@
+export const TASKS = [
+	{
+		id: 1,
+		title: "Aprender JavaScript",
+		subject: "JavaScript",
+		priority: "high",
+        completed: false,
+	},
+	{
+		id: 2,
+		title: "Estudiar inglés",
+		subject: "Idiomas",
+		priority: "medium",
+        completed: false,
+	},
+	{
+		id: 3,
+		title: "Hacer la cama",
+		subject: "Personal",
+		priority: "medium",
+        completed: false,
+	},
+	{
+		id: 4,
+		title: "Ir al súper",
+		subject: "Personal",
+		priority: "medium",
+        completed: false,
+	},
+	{
+		id: 5,
+		title: "Revisar correos",
+		subject: "Personal",
+		priority: "low",
+        completed: false,
+	},
+];
